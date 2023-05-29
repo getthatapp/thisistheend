0198a1e359a723ad6df2effd1f7b0ce4243044c5
Grzegorz Turek <g.turek@yahoo.com>
2023-05-29 22:50:37 +0200

    form, new_post, nav

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
@@ -12,7 +12,7 @@ def index(page):
     return render_template('entries.html', entries=entries)
 
 
-@app.route('/new-post/', methods=['GET', 'POST'])
+@app.route('/new_post/', methods=['GET', 'POST'])
 def create_entry():
     form = EntryForm()
     errors = None
@@ -25,6 +25,6 @@ def create_entry():
             )
             db.session.add(entry)
             db.session.commit()
-        else:
-            errors = form.errors
-    return render_template('entry_form.html', form=form, errors=errors)
+            return redirect(url_for('entries'))
+    elif request.method == 'GET':
+        return render_template('new_post.html', form=form)
