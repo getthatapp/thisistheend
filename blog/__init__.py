ea97141847abc57398aed014fc8cb1a0c96b2719
Grzegorz Turek <g.turek@yahoo.com>
2023-06-03 15:35:39 +0200

    koniec aplikacji

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,24 +4,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+# tworzymy instancje klasy Flask
 app = Flask(__name__)
+# ladujemy konfiguracje aplikacji z klasy Config
 app.config.from_object(Config)
+# tworzymy instancję SQLAlchemy, która reprezentuje bazę danych
 db = SQLAlchemy(app)
+# tworzymy instancję Migrate, która umożliwia wykonywanie migracji bazy danych
 migrate = Migrate(app, db, render_as_batch=True)
+# tworzymy instancję LoginManager, która ułatwia zarządzanie sesjami użytkowników
 login = LoginManager(app)
+# określamy nazwę widoku, do którego użytkownik zostanie przekierowany,
+# jeśli nie jest zalogowany i próbuje uzyskać dostęp do zasobów wymagających uwierzytelnienia
 login.login_view = 'login'
 
 from blog import routes, models
 from blog import fake_data
 
 
-@login.user_loader
+@login.user_loader  # dekorator
+# Ta funkcja jest używana do ładowania obiektu użytkownika na podstawie jego identyfikatora
 def load_user(user_id):
     from blog.models import User
     return User.query.get(int(user_id))
 
 
-@app.shell_context_processor
+@app.shell_context_processor # dekorator
+# a funkcja zwraca słownik, który definiuje kontekst powłoki.
 def make_shell_context():
     return {
         "db": db,
@@ -29,7 +38,7 @@ def make_shell_context():
     }
 
 
-@app.cli.command('generate-fake-data')
+@app.cli.command('generate-fake-data') # dekorator, który rejestruje funkcję jako komendę CLI (Command Line Interface
 def generate_fake_data_command():
     user_id = input("Enter user ID: ")
     how_many = input("How many entries create?: ")
--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 from werkzeug.urls import url_parse
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from blog import app, db
 from blog.models import Entry, User
 from blog.forms import EntryForm, RegistrationForm, LoginForm
@@ -14,7 +14,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Thanks for registering! Now you can login!')
+        flash('Thanks for registering! Now you can login!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
@@ -27,9 +27,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid email or password!')
+            flash('Invalid email or password!', 'danger')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        flash('`you have successfully logged in.', 'success')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -40,31 +41,37 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
+    flash('You have been logged out successfully', 'success')
     return redirect(url_for('index'))
-
 
 
 @app.route('/', defaults={'page': 1})
 @app.route('/page/<int:page>')
 def index(page):
-    PER_PAGE = 5
+    PER_PAGE = 15
     entries = Entry.query.order_by(Entry.pub_date.desc()).paginate(page=page, per_page=PER_PAGE, error_out=False)
     return render_template('entries.html', entries=entries)
 
 
 @app.route('/new_post/', methods=['GET', 'POST'])
+@login_required
 def create_entry():
     form = EntryForm()
     errors = None
     if request.method == 'POST':
         if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
+            try:
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
+                db.session.commit()
+                flash('Your post was successfully added!', 'success')
+            except:
+                flash('There was an error adding your post. Please try again.', 'danger')
+                db.session.rollback()
             return redirect(url_for('index'))
     elif request.method == 'GET':
         return render_template('new_post.html', form=form)
@@ -77,12 +84,32 @@ def entry_detail(entry_id):
 
 
 @app.route('/edit_post/<int:entry_id>', methods=['POST', 'GET'])
+@login_required
 def edit_entry(entry_id):
     entry = Entry.query.get_or_404(entry_id)
     form = EntryForm(obj=entry)
     if request.method == "POST":
         if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
+            try:
+                form.populate_obj(entry)
+                db.session.commit()
+                flash('Your post has been updated successfully!', 'success')
+            except:
+                flash('There was an error updating your post. Try again.', 'danger')
+                db.session.rollback()
             return redirect(url_for('entry_detail', entry_id=entry.id))
     return render_template('edit_entry.html', form=form, entry_id=entry_id)
+
+
+@app.route('/delete_post/<int:entry_id>', methods=['POST'])
+@login_required
+def delete_entry(entry_id):
+    entry = Entry.query.get_or_404(entry_id)
+    try:
+        db.session.delete(entry)
+        db.session.commit()
+        flash("Your post has been successfully deleted!", 'success')
+    except:
+        db.session.rollback()
+        flash('There was an error deleting your post. Please try again.', 'danger')
+    return redirect(url_for('index'))
